79216953ad9dcc30b5d66c32f43d5afde946c1f4
Vitia kuramawhynot <egorbelka2000@gmail.com>
2024-10-11 16:24:24 +0300

    Third task

---
+++ Lesson_1/Task_3/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout << "Введите 2 числа:\n ";
+    cin >> a >> b;
+
+    cout << "Выберите математическое действие: " <<
+        "\n1. Сложение" <<
+        " \n2. Вычетание" <<
+        " \n3. Деление" <<
+        " \n4. Умножение\n ";
+    cin >> c;
+    switch (c)
+    {
+    case 1:
+        cout <<"Результат сложения: "<< a + b<<endl;
+        break;
+    case 2:
+        cout <<"Результат вычетания: "<< a - b<<endl;
+        break;
+    case 3:
+        cout <<"Результат деления: "<<(double)a / b<<endl;
+        break;
+    case 4:
+        cout <<"Результат умножения: "<< a * b<<endl;
+        break;
+    default:
+        cout << "Ошибка! Пункт Меню отсудствует!"<<endl;
+        break;
+    }
+}
